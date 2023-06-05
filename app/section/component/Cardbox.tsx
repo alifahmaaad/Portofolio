7ea24739b3fbe6e892a2cb218a59795e33b210a2
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-06-06 06:36:10 +0700

    update src img 3

--- app/section/component/Cardbox.tsx
+++ app/section/component/Cardbox.tsx
@@ -2,7 +2,7 @@ const Cardbox = (props: { text: string; title?: string; imgurl?: string }) => {
   return (
     <div className="bg-white text-gray-700 text-xs sm:text-normal w-[10rem] md:w-72 min-h-[5rem] md:min-h-[10rem] shadow-lg rounded-md overflow-hidden">
       <img
-        src={"https://alifahmaaad.github.io/Portofolio/" + props.imgurl}
+        src={"https://alifahmaaad.github.io/Portofolio" + props.imgurl}
         className="w-full h-[5rem] md:h-[10rem]"
       />
       <div className="p-2 px-4">
