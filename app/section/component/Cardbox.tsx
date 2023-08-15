64ffbb860299720c8b68bc332dc720278a2dfd63
Ahmad Alif Sofian <alif12sofian@gmail.com>
2023-08-15 10:00:35 +0700

    add mealapp

--- app/section/component/Cardbox.tsx
+++ app/section/component/Cardbox.tsx
@@ -8,7 +8,7 @@ const Cardbox = (props: {
 }) => {
   return (
     <div className="bg-white text-gray-700 text-xs md:text-normal w-72 h-[16rem] sm:h-auto md:min-h-[10rem] shadow-lg rounded-md overflow-hidden zoom relative">
-      <a href={props.link}>
+      <a href={props.link} target="blank">
         {props.link && (
           <div className="absolute flex items-center justify-center w-full h-full opacity-0 hover:opacity-100 bg-opacity-60 bg-black text-white font-bold text-xl">
             {props.linktext}
